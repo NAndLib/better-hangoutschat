d451196a2dbb2c7dd5b5a1c33eb0d5fea3904a6a
Andrew Tran <andremail03@gmail.com>
2022-09-07 11:35:30 -0700

    Fixup extension script

--- generate_ff_extention.sh
+++ generate_ff_extention.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 OUT_DIR="./out/ghctheme/firefox"
-FF_SECRET="~/.secrets/firefox_addon_JWT"
-web-ext build "$OUT_DIR"
-web-ext sign --api-key $(head -1 "$FF_SECRET") --api-secret $(tail -f "$FF_SECRET")
+FF_SECRET="$HOME/.secrets/firefox_addon_JWT"
+
+./generate_patch.sh
+
+pushd "$OUT_DIR"
+web-ext build --overwrite-dest
+web-ext sign --api-key $(head -1 "$FF_SECRET") --api-secret $(tail -1 "$FF_SECRET")
+popd
